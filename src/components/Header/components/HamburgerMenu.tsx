47f89f377274c7c83f8dd1b3a3e6cb971145009b
Svitlana Mishchuk <svitlana@igtechn.com>
2022-07-05 15:54:03 +0200

    feat(navigation), added mobile navigation

--- src/components/Header/components/HamburgerMenu.tsx
+++ src/components/Header/components/HamburgerMenu.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 
 import { IconButton } from '../../../shared/components/IconButton';
+import { Logo } from './Logo';
+import { Navigation } from './Navigation';
 
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
@@ -15,13 +17,23 @@ export const HamburgerMenu = () => {
   return (
     <>
       <IconButton icon={faBars} onClick={handleHamburgerMenuClick} />
+      {/*   Mobile navigation */}
       {isMenuOpen && (
         <div className='hamburger-menu_container'>
-          <IconButton
-            className='hamburger-menu_close'
-            icon={faXmark}
-            onClick={handleHamburgerMenuClick}
-          />
+          <div className='flex'>
+            <IconButton
+              className='hamburger-menu_close'
+              icon={faXmark}
+              onClick={handleHamburgerMenuClick}
+            />
+            <Logo className='hamburger-menu_logo' />
+          </div>
+          <div>
+            <Navigation
+              containerClassName={'direction-column'}
+              navigationItemClassName={'hamburger-menu_item'}
+            />
+          </div>
         </div>
       )}
     </>
