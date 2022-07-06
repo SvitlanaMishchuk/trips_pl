59df27ab5891ce758ee428b69e97341e985deef1
Svitlana Mishchuk <svitlana@igtechn.com>
2022-07-06 20:11:36 +0200

    refactor(styles), added and improved mobile view for HowItWorks component

--- src/components/HowItWorks/index.tsx
+++ src/components/HowItWorks/index.tsx
@@ -5,7 +5,7 @@ import './styles.scss';
 export const HowItWorks = () => {
   return (
     <div className='how-it-works_container'>
-      <h2 className='text-center'>How It Works</h2>
+      <h1 className='text-center'>How It Works</h1>
       <div className='flex justify-space-between how-it-works_content'>
         {steps.map(({ title, content }: Step) => (
           <Card
