ff3a212a18415581d5111d9e18d6e8e03dd1467e
Svitlana Mishchuk <svitlana@igtechn.com>
2022-07-07 10:36:49 +0200

    refactor(shared components), added opportunity to add image and updated Card component

--- src/shared/components/Card/index.tsx
+++ src/shared/components/Card/index.tsx
@@ -1,19 +1,22 @@
 import './styles.scss';
 
 interface CardProps {
-  title?: string;
+  title: string;
   content?: string;
+  image?: string;
   containerClassname?: string;
 }
 
 export const Card = ({
   title,
   content,
+  image,
   containerClassname = '',
 }: CardProps) => {
   return (
     <div className={`card_container text-center ${containerClassname} `}>
-      {title && <h3>{title}</h3>}
+      {image && <img className={`card_image`} src={image} />}
+      <h3 className={`card_title`}>{title}</h3>
       {content && <p className={'card_content'}>{content}</p>}
     </div>
   );
